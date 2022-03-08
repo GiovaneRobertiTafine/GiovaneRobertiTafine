e01e0e27e71a9c39eb2f85705b25d0026b58f8cf
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-03-08 12:56:11 -0300

    Alterando arquivo index

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ async function main() {
     const readmeTemplate = (
         await fs.readFile(path.join(process.cwd(), "./README.template.md"))
     ).toString("utf-8");
+
+    const office_quote = await (
+        await fetch("https://officeapi.dev/api/quotes/random")
+    ).json();
+
+    const readme = readmeTemplate
+        .replace("{office_quote}", office_quote.data.content)
+        .replace("{office_character}", `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`);
+
+
+    await fs.writeFile("README.md", readme);
 }
 main();
-
-const office_quote = await(
-    await fetch("https://officeapi.dev/api/quotes/random")
-).json();
-
-const readme = readmeTemplate
-    .replace("{office_quote}", office_quote.data.content)
-    .replace("{office_character}", `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`);
-
-
-await fs.writeFile("README.md", readme);

6e8fedb712111dab8513a005cbb6cb4abd97fa00
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2023-07-04 10:42:43 -0300

    Inserindo test work

---
+++ workflow.js
@@ -0,0 +1,23 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function main() {
+    const readmeTemplate = (
+        await fs.readFile(path.join(process.cwd(), "./.github/workflows/dynamic-injection-workflow.yml"))
+    ).toString("utf-8");
+
+    const index = readmeTemplate
+        .indexOf("0 0 */3 * 1-5");
+
+    if (index !== -1) {
+        readmeTemplate.replace("0 0 */3 * 1-5", "0 0 */4 * 1-5");
+    } else {
+        readmeTemplate.replace("0 0 */4 * 1-5", "0 0 */3 * 1-5");
+    }
+
+    const readme = readmeTemplate;
+
+    await fs.writeFile("./.github/workflows/dynamic-injection-workflow.yml", readme);
+}
+main();
